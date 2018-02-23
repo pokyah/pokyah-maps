fe630e343df45e70de808189bf87b42ac36b624a
pokyah <thomas.goossens@protonmail.com>
2018-02-23 12:44:35 +0100

    updated index

--- index-html.sh
+++ index-html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
- 
-echo '<html><body>'
+
+echo '<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>'
 sed 's/^.*/<a href="&">&<\/a><br\/>/'
 echo '</body></html>'
 
